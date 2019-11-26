cd60f48c5ea16f46c0867debbbcccc495c95eadf
unknown <vincekieft1997@gmail.com>
2019-11-26 16:51:25 +0100

    Updated export

--- lib/http_manager.dart
+++ lib/http_manager.dart
@@ -4,5 +4,11 @@
 library http_manager;
 
 export 'src/http_manager.dart';
+export 'src/adapters/base_adapters/persistent_connection_adapter.dart';
+export 'src/adapters/http_adapter.dart';
+export 'src/handlers/retry_exception_handler.dart';
+export 'src/dto/request.dart';
+export 'src/dto/response.dart';
+export 'src/interfaces/i_exception_handler.dart';
+export 'src/interfaces/i_http_adapter.dart';
 
-// TODO: Export any libraries intended for clients of this package.
